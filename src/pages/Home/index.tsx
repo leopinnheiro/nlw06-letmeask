dc70584d73242e162d2dc40f0c5efdd09a58aad0
Leonardo Pinheiro <leo.pinnheiro@gmail.com>
2021-06-21 23:10:40 -0300

    Autenticaçao com firebase

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,6 @@
 import { useHistory } from "react-router-dom";
+import { auth, firebase } from "../../services/firebase";
+
 import illustrationImg from '../../assets/images/illustration.svg';
 import logoImg from '../../assets/images/logo.svg';
 import googleIconImg from '../../assets/images/google-icon.svg';
@@ -9,8 +11,14 @@ import './styles.scss';
 export function Home() {
   const history = useHistory();
 
-  function navigateToNewRoom() {
-    history.push('rooms/new')
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider)
+      .then(result => {
+        console.log(result);
+        history.push('rooms/new');
+      });
   }
 
   return (
@@ -25,7 +33,7 @@ export function Home() {
           <img src={logoImg} alt="Letmeask" />
           <button
             className="create-room"
-            onClick={navigateToNewRoom}
+            onClick={handleCreateRoom}
           >
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
